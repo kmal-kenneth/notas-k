85c42bf7be546a3bdd85283b5d00597e4b4d506b
Kenet Mauricio Acuña Lago <kmal.kenneth@gmail.com>
2021-11-26 14:50:54 -0600

    add cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -19,7 +19,11 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter()
+		adapter: cloudflare({
+			// any esbuild options
+			target: 'es2020',
+			platform: 'browser'
+		})
 	}
 };
 
