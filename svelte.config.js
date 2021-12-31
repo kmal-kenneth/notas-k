15a0406b4e93b2ea02ad02523290569eb4440295
Kenet Mauricio Auña Lago <kmal.kenneth@gmail.com>
2021-12-31 13:25:45 -0600

    delete mdx

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,16 @@
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import cloudflare from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	extensions: ['.svelte'],
 
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
 		preprocess({
 			postcss: true
-		}),
-		mdsvex(mdsvexConfig)
+		})
 	],
 
 	kit: {
