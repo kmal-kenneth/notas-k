fbb55e32f2bdced57bed84c87a9b84ee44a76f89
Kenet Mauricio Acuña Lago <kmal.kenneth@gmail.com>
2022-02-22 13:17:18 -0600

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import cloudflare from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,8 +16,10 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 
-		adapter: cloudflare({
-			// any esbuild options
+		adapter: adapter({
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app
+			split: false
 		}),
 		vite: {
 			build: {
