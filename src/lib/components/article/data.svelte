f2c4d0678354e82906f9edd81131573811b887b9
Kenet Acuña <kmal.kenneth@gmail.com>
2022-06-29 14:47:39 -0600

    update style data article

--- src/lib/components/article/data.svelte
+++ src/lib/components/article/data.svelte
@@ -3,7 +3,7 @@
 	export let subtitle: string;
 </script>
 
-<div class="flex flex-col items-start my-2 mx-4">
+<div class="flex flex-col items-start my-2 mx-2 sm:mx-4">
 	<p class="m-0 lg:m-0 font-bold">
 		{title}
 	</p>
